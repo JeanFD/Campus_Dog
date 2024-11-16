5ee9629b93936698fc031cfb5d0c2816be7c9de0
Jean Dias <102756457+JeanFD@users.noreply.github.com>
2024-11-16 11:25:39 -0300

    WIP: separar nav das páginas, permitindo maior facilidade na manutenção

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -16,16 +16,35 @@ $('#animalModal').on('show.bs.modal', function (event) {
     modal.find('#animalDescricao').text(descricao);
     modal.find('#animalFoto').attr('src', foto);  // Atualiza a foto no modal
 });
-
 document.addEventListener("DOMContentLoaded", function () {
     // Esconde o loader após o carregamento completo da página
-    const loader = document.getElementById('loader');
-    loader.style.display = 'none';
+    // const loader = document.getElementById('preloader');
+    // if(loader){
+    //     loader.style.display = 'none';
+    // }
+
+   
+
+    const currentPath = window.location.pathname;
+
+    // Seleciona todos os links do menu
+    const menuLinks = document.querySelectorAll(".navbar-nav .nav-link");
+
+    console.log(currentPath, menuLinks);
+
+    // Itera pelos links e adiciona a classe 'active' ao correspondente
+    menuLinks.forEach(link => {
+        console.log(link.getAttribute("href") );
+        console.log(currentPath)
+        if (link.getAttribute("href") === currentPath) {
+            link.classList.add("active");
+        }
+    });
 
     // Exibe o loader ao enviar formulários
-    document.querySelectorAll('form').forEach(form => {
-        form.addEventListener('submit', function () {
-            loader.style.display = 'flex';
-        });
-    });
+    // document.querySelectorAll('form').forEach(form => {
+    //     form.addEventListener('submit', function () {
+    //         loader.style.display = 'flex';
+    //     });
+    // });
 });
