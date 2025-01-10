6fdafe73a8358b5b1a0c2d6cd9667adbd9a30239
Jean Dias <102756457+JeanFD@users.noreply.github.com>
2025-01-09 22:05:38 -0300

    versão final 1

--- app/urls.py
+++ app/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('doacoes', views.doacoes, name='doacoes'),
     path('saiba_mais', views.saiba_mais, name='saiba_mais'),
     path('contato', views.contato, name='contato'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
